b0187a50764355d4edded7fd47198f1ae16533fa
Edelmiro Anton <anton.edelmiro@gmail.com>
2025-01-01 21:20:45 +0100

    add img and name profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import './App.css'
+import "./App.css";
+import profile_img from "../assets/images/avatar-jessica.jpeg";
 
 function App() {
-
   return (
     <>
-    <div className='bg-cyan-300'>Test Tailwind</div>
+      <main className="bg-backgroundCard w-[327px] h-[501px] rounded-2xl ">
+        <div className="mt-6">
+          <img
+            src={profile_img}
+            alt="profile image"
+            className="rounded-full m-auto w-[80px]"
+          />
+        </div>
+
+        <p className="text-white text-center text-[1.125rem] font-semibold mt-6">
+          Jessica Randall
+        </p>
+        <p className="text-fontGreen text-center text-[0.625rem] font-semibold mt-1">
+          London, United Kingdom
+        </p>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
