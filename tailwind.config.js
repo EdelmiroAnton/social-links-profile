ce631976a2b5802dfb461310814360389a5b3192
Edelmiro Anton <anton.edelmiro@gmail.com>
2025-01-01 21:07:01 +0100

    add green font color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ export default {
       colors: {
         backgroundGray: "#141414",
         backgroundCard: "#1f1f1f",
-        backgroundLinks: "#333333"
+        backgroundLinks: "#333333",
+        fontGreen: "#c5f82a",
       },
     },
   },
