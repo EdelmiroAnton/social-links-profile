1dd7ee41efd0d486fc389c760688ce0a9106fc5c
Edelmiro Anton <anton.edelmiro@gmail.com>
2025-01-02 20:26:07 +0100

    add section tag

--- src/App.tsx
+++ src/App.tsx
@@ -20,16 +20,17 @@ function App() {
             className="rounded-full m-auto w-[80px]"
           />
         </div>
-
-        <p className="text-white text-center text-[1.4375rem] font-normal mt-6">
-          Jessica Randall
-        </p>
-        <p className="text-fontGreen text-center text-[0.8125rem] font-medium mt-1">
-          London, United Kingdom
-        </p>
-        <p className=" text-gray-300 text-center text-[0.8125rem] font-normal mt-5">
-          "Front-end developer and avid reader."
-        </p>
+        <section>
+          <p className="text-white text-center text-[1.4375rem] font-normal mt-6">
+            Jessica Randall
+          </p>
+          <p className="text-fontGreen text-center text-[0.8125rem] font-semibold mt-1">
+            London, United Kingdom
+          </p>
+          <p className=" text-gray-300 text-center text-[0.8125rem] font-normal mt-5">
+            "Front-end developer and avid reader."
+          </p>
+        </section>
         <ul className="max-w-full w-[270px] mx-auto mt-6">
           {links.map((el) => (
             <li className="bg-backgroundLinks text-white text-center text-[0.75rem] font-semibold mt-4 mb-3 p-1 rounded-lg h-10 content-center">
